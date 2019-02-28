4547681378959d754101e6f08ceca0bd555e4bef
Masaaki Uno <unokun@gmail.com>
2019-03-01 07:12:36 +0900

    add area search

--- model/area.go
+++ model/area.go
@@ -5,28 +5,71 @@ import "github.com/unokun/gc_alert/db"
 /*
 県情報
 */
-type Pref struct {
+type Area struct {
 	PrefID   string `json:"pref_id"`
 	PrefName string `json:"pref_name"`
-}
-
-/**
- */
-func (p *Pref) TableName() string {
-	return "areas"
+	CityID   string `json:"city_id"`
+	CityName string `json:"city_name"`
+	TownID   string `json:"town_id"`
+	TownName string `json:"town_name"`
 }
 
 /**
 県一覧を検索する
 */
-func findAllPrefs() (*[]Pref, error) {
+func FindAllPrefs() (*[]Area, error) {
 	db, err := db.Connect()
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
-	prefs := []Pref{}
-	result := db.Exec("SELECT DISTINCT pref_id, pref_name FROM areas ORDER BY pref_id").Scan(&prefs)
+	prefs := []Area{}
+	result := db.Raw("SELECT DISTINCT pref_id, pref_name FROM areas ORDER BY pref_id").Scan(&prefs)
+	return &prefs, result.Error
+}
+
+/**
+市一覧を検索する
+*/
+func FindCities(prefID int) (*[]Area, error) {
+	db, err := db.Connect()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	prefs := []Area{}
+	result := db.Raw("SELECT DISTINCT city_id, city_name FROM areas WHERE pref_id = ? ORDER BY city_id", prefID).Scan(&prefs)
+	return &prefs, result.Error
+}
+
+/**
+町一覧を検索する
+*/
+func FindTowns(cityID int) (*[]Area, error) {
+	db, err := db.Connect()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	prefs := []Area{}
+	result := db.Raw("SELECT DISTINCT town_id, town_name FROM areas WHERE city_id = ? ORDER BY town_id", cityID).Scan(&prefs)
+	return &prefs, result.Error
+}
+
+/**
+地域を検索する
+*/
+func FindAreaByZipCd(zipCD string) (*[]Area, error) {
+	db, err := db.Connect()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	prefs := []Area{}
+	result := db.Where("zip_cd = ?", zipCD).Find(&prefs)
 	return &prefs, result.Error
 }
--- model/area_test.go
+++ model/area_test.go
@@ -3,15 +3,52 @@ package model
 import "testing"
 
 func TestFindAllPrefs(t *testing.T) {
-	prefs, err := findAllPrefs()
+	areas, err := FindAllPrefs()
 	if err != nil {
 		t.Fatalf("failed test %#v", err)
 	}
-	if len(*prefs) == 0 {
+	if len(*areas) == 0 {
 		t.Fatalf("failed test %#v", err)
 	}
-	for _, p := range *prefs {
-		println("pref_id: " + p.PrefID)
-		println("pref_name: " + p.PrefName)
+	for _, a := range *areas {
+		println("pref: " + a.PrefID + " " + a.PrefName)
+	}
+}
+func TestFindCities(t *testing.T) {
+	areas, err := FindCities(1)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if len(*areas) == 0 {
+		t.Fatalf("failed test %#v", err)
+	}
+	for _, a := range *areas {
+		println("city: " + a.CityID + " " + a.CityName)
+	}
+}
+func TestFindTowns(t *testing.T) {
+	areas, err := FindTowns(1101)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if len(*areas) == 0 {
+		t.Fatalf("failed test %#v", err)
+	}
+	for _, a := range *areas {
+		println("town: " + a.TownID + " " + a.TownName)
+	}
+}
+func TestFindAreaByZipCd(t *testing.T) {
+	areas, err := FindAreaByZipCd("703-8256")
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	if len(*areas) == 0 {
+		t.Fatalf("failed test %#v", err)
+	}
+	for _, a := range *areas {
+		println("pref: " + a.PrefID + " " + a.PrefName)
+		println("city: " + a.CityID + " " + a.CityName)
+		println("town: " + a.TownID + " " + a.TownName)
 	}
 }
