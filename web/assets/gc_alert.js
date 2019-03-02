584f3fdbb6c3e267ed25b051d1136e8ecf2e2dc1
Masaaki Uno <unokun@gmail.com>
2019-03-02 12:19:45 +0900

    add area check before registering LINE notify

--- web/assets/gc_alert.js
+++ web/assets/gc_alert.js
@@ -1,7 +1,5 @@
 function searchArea() {
-    alert('searchArea');
     let zip_cd = document.getElementById('zip_cd').value;
-    alert(zip_cd);
     if (zip_cd === "") {
         alert("郵便番号を入力してください。");
         return;
@@ -32,4 +30,13 @@ function searchArea() {
     }
     req.open('GET', '/gc_alert/search_area?zip_cd=' + zip_cd, true);
     req.send(null);
+}
+
+function checkLineNotifyUrl() {
+    let url = document.getElementById('notify_url').href;
+    if (url.endsWith('=')) {
+        alert('地域検索を行ってください。');
+        return false;
+    }
+    return true;
 }
